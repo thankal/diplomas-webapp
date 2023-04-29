2b6159f879966a751733529f2eef678088b36207
thankal <thanasiskal81@gmail.com>
2023-04-29 15:47:59 +0300

    Fixed registration required field bug

--- thesisapp/src/main/resources/static/js/signup.js
+++ thesisapp/src/main/resources/static/js/signup.js
@@ -2,10 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
     var roleSelect = document.querySelector('select[name="role"]');
     var studentFields = document.querySelector('.student-fields');
     var professorFields = document.querySelector('.professor-fields');
+
+    studentInputs = studentFields.querySelectorAll("input");
+    professorInputs = professorFields.querySelectorAll("input");
+
   
     // Hide student fields by default
     studentFields.style.display = 'none';
     professorFields.style.display = 'none';
+
 
     // in the roleSelect select the disabled option
     roleSelect.selectedIndex = 0;
@@ -15,10 +20,26 @@ document.addEventListener("DOMContentLoaded", function() {
       if (roleSelect.value === 'STUDENT') {
         studentFields.style.display = 'flex';
         professorFields.style.display = 'none';
+        
+          // enable only the student specific fields
+          for (var i = 0; i< professorInputs.length; i++) {
+            professorInputs[i].disabled = true;
+          }
+          for (var i = 0; i< studentInputs.length; i++) {
+            studentInputs[i].disabled = false;
+          }
       } 
       else if (roleSelect.value === 'PROFESSOR') {
           studentFields.style.display = 'none';
           professorFields.style.display = 'flex';
+
+          // enable only the professor specific fields
+          for (var i = 0; i< studentInputs.length; i++) {
+            studentInputs[i].disabled = true;
+          }
+          for (var i = 0; i< professorInputs.length; i++) {
+            professorInputs[i].disabled = false;
+          }
       }else {
         studentFields.style.display = 'none';
         professorFields.style.display = 'none';
